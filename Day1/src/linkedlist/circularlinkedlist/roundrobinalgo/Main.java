b762ad86efaf89687c3e37a5cdfa607ca67e5f07
akshit-patel23 <akshitpatel.g@gmail.com>
2025-01-30 10:53:50 +0530

    Library Management System

---
+++ Day1/src/linkedlist/circularlinkedlist/roundrobinalgo/Main.java
@@ -0,0 +1,21 @@
+package linkedlist.circularlinkedlist.roundrobinalgo;
+
+public class Main {
+    public static void main(String[] args) {
+        Process p=new Process();
+
+        p.addAttail(10,3,3);
+        p.addAttail(5,6,5);
+        p.addAttail(8,13,5);
+        p.addAttail(9,3     ,4);
+
+
+
+
+        System.out.println("Execution------");
+        p.execution();
+
+
+    }
+
+}
