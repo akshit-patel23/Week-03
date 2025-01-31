8e7aa03160985073a072d7e0e29b310eb7164a7d
akshit-patel23 <akshitpatel.g@gmail.com>
2025-01-31 23:07:22 +0530

    Two Sum Problem

---
+++ Day2/src/hashmapshashfunctions/twosumproblem/Main.java
@@ -0,0 +1,15 @@
+package hashmapshashfunctions.twosumproblem;
+import hashmapshashfunctions.pairwithgivensuminanarray.PairSum;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int arr[]={2,1,3,3,4,3};
+        int given=6;
+        PairSum par=new PairSum();
+        par.pair(arr,given);
+        par.display();
+
+    }
+}
+
