3024f8f8dc03f0c414053e2dd106856c028ab87f
akshit-patel23 <akshitpatel.g@gmail.com>
2025-01-31 21:58:26 +0530

    Stock Span using Stack

---
+++ Day2/src/stackandqueue/stockspanproblem/Main.java
@@ -0,0 +1,10 @@
+package stackandqueue.stockspanproblem;
+
+public class Main {
+    public static void main(String[] args) {
+        StockSpan s=new StockSpan();
+        int[] prices = {100, 80, 60, 70, 60, 75, 85};  // Example stock prices
+        s.calculate(prices);
+        s.display();
+    }
+}
