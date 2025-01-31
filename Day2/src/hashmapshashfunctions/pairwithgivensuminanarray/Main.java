fabaaffeba56f22f04bd843aa0fee33868b6679c
akshit-patel23 <akshitpatel.g@gmail.com>
2025-01-31 23:10:46 +0530

    pair with given sum in an array

---
+++ Day2/src/hashmapshashfunctions/pairwithgivensuminanarray/Main.java
@@ -0,0 +1,12 @@
+package hashmapshashfunctions.pairwithgivensuminanarray;
+
+public class Main {
+    public static void main(String[] args) {
+        int arr[]={2,1,3,3,4,3};
+        int given=6;
+        PairSum par=new PairSum();
+        par.pair(arr,given);
+        par.display();
+
+    }
+}
