7f6ae91cec1f36449664963f3d272cc5191d1f5e
akshit-patel23 <akshitpatel.g@gmail.com>
2025-01-31 23:09:25 +0530

    Subarrays With Zero Sum

---
+++ Day2/src/hashmapshashfunctions/subarrayswithzerosum/Main.java
@@ -0,0 +1,12 @@
+package hashmapshashfunctions.subarrayswithzerosum;
+
+public class Main {
+    public static void main(String[] args) {
+        int arr[]={-2,1,1,3,-4,3};
+        SubArray sub=new SubArray();
+        sub.pair(arr);
+        sub.display();
+
+    }
+}
+
