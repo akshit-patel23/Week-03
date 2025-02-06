3a64813897a8e215287bda0e09f2cf20067a84a5
akshit-patel23 <akshitpatel.g@gmail.com>
2025-02-06 11:30:15 +0530

    [Akshit] Add . Find the First and Last Occurrence of an Element in a Sorted Array using Binary Search with its test file

---
+++ Day4/src/test/java/binarysearchtest/firstandlastoccurencetest/FirstAndLastOccurenceTest.java
@@ -0,0 +1,26 @@
+package binarysearchtest.firstandlastoccurencetest;
+
+import binarysearch.firstandlastoccurence.FirstAndLastOccurence;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class FirstAndLastOccurrenceTest {
+
+    @Test
+    public void tester(){
+        FirstAndLastOccurrence firstlast=new FirstAndLastOccurrence();
+        int[] arr={1,1,1,1,3,3,3,4,5,5,6,7,8,9};
+        int target=1;
+        int index=firstlast.binarySearch(arr,target);
+        int first=firstlast.firstOccurance(arr,index);
+        int last=firstlast.lastOccurance(arr,index);
+
+        Assert.assertEquals(0,first);
+        Assert.assertEquals(3,last);
+
+    }
+
+
+}
